40a193e9e6255f9f40650b30ba9b9be54e7c4447
vzhomeexperiments <vz.home.experiments@gmail.com>
2017-08-19 09:10:20 +0200

    re-organized inputs into single elements; merged table of P/L with time series graph

--- ui.R
+++ ui.R
@@ -8,44 +8,28 @@ library(shinydashboard)
 library(shiny)
 # 
 dashboardPage(
-  dashboardHeader(title = "Trading Systems performance evalutaiton"),
-  dashboardSidebar(),
+  dashboardHeader(title = "fxtrams"),
+  dashboardSidebar(
+      # Elements on the Sidebar of the App
+      selectInput(inputId = "TermNum", label = "Select Terminal Number",choices = 1:4),
+      actionButton(inputId = "Refresh", label = "refresh"),
+      selectInput(inputId = "MagicNum", label = "Select Magic Number", choices = 1:10),
+      sliderInput(inputId = "filter", label = "Select Profit Levels", min = -10000, max = 10000, value = c(0, 10000)),
+      dateInput(inputId = "filterDate", label = "Select Orders newer than...", value = "2017-02-01"),
+      sliderInput(inputId = "nTrades", label = "Select Orders number greater than...", value = c(0, 100),min = 0, max = 100)
+  ),
   dashboardBody(
-    fluidRow(
-      column(1, selectInput(inputId = "TermNum", label = "Select Terminal Number",choices = 1:4)),
-      column(1, actionButton(inputId = "Refresh", label = "refresh")),
-      column(1, selectInput(inputId = "MagicNum", label = "Select Magic Number", choices = 1:10)),
-      column(2, sliderInput(inputId = "filter", label = "Select Profit Levels", min = -10000, max = 10000, value = c(0, 10000))),
-      column(1, dateInput(inputId = "filterDate", label = "Select Orders newer than...", value = "2017-02-01")),
-      column(1, sliderInput(inputId = "nTrades", label = "Select Orders number greater than...", value = c(0, 100),min = 0, max = 100))
-    ),
-    hr(),
-    mainPanel(
-        tabsetPanel(
-          tabPanel("All Systems Plot", plotOutput('plot1')),
-          tabPanel("Basic Statistics", tableOutput('statistics')),
-          tabPanel("Visuals of Selected System", plotOutput("plot2"))
-        )  
-    )
+        
+      mainPanel(
+        # Elements of the Dashboard: header and tabset panel
+        headerPanel("Trading Systems Graphical performance overview"),
+          tabsetPanel(
+            # Default chart visualizing the overall performance of the systems
+            tabPanel("All Systems Plot", plotOutput('plot1')),
+            # table and graph visualizing statistical performance and time-series graph
+            tabPanel("Basic Statistics and Graph", tableOutput('statistics'), plotOutput("plot2"))
+          )  
+      )
   )
 )
-
-
-# 
-# pageWithSidebar(
-#   headerPanel('Systems performance evalutaiton'),
-#   sidebarPanel(
-#     selectInput(inputId = "TermNum", label = "Select Terminal Number",choices = 1:4),
-#     actionButton(inputId = "Refresh", label = "refresh"),
-#     selectInput(inputId = "MagicNum", label = "Select Magic Number", choices = 1:10),
-#     sliderInput(inputId = "filter", label = "Select Profit Levels", min = -10000, max = 10000, value = c(0, 10000)),
-#     dateInput(inputId = "filterDate", label = "Select Orders newer than...", value = "2017-02-01"),
-#     sliderInput(inputId = "nTrades", label = "Select Orders number greater than...", value = c(0, 100),min = 0, max = 100)
-#     ),
-#   
-#   mainPanel(
-#     plotOutput('plot1'), # overall visual representation of the terminal trades
-#     tableOutput('statistics'), # statistics of selected system
-#     plotOutput("plot2") # visual representation of selected system
-#   )
-# )
+# end of ui.R

0fd6c73f4f6de05f30f62dd585a7014022949236
vzhomeexperiments <vz.home.experiments@gmail.com>
2017-08-19 08:42:25 +0200

    App transferred into Shiny Dashboard style This commit is intermediate just make sure this is still working

--- ui.R
+++ ui.R
@@ -1,30 +1,51 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
+# Project to create easy to use Trading Journal adapted to personal use
+# Help to develop, demo-test Automated Trading Systems
 #
-# http://shiny.rstudio.com
-#
+# (C) 2017 Vladimir Zhbanko github: vzhomeexperiments/trademonitor
 
-
+# ui.R
+library(shinydashboard)
 library(shiny)
-
-
-
-
-pageWithSidebar(
-  headerPanel('Systems performance evalutaiton'),
-  sidebarPanel(
-    selectInput(inputId = "TermNum", label = "Select Terminal Number",choices = 1:4),
-    actionButton(inputId = "Refresh", label = "refresh"),
-    selectInput(inputId = "MagicNum", label = "Select Magic Number", choices = 1:10),
-    sliderInput(inputId = "filter", label = "Select Profit Levels", min = -10000, max = 10000, value = c(0, 10000)),
-    dateInput(inputId = "filterDate", label = "Select Orders newer than...", value = "2017-02-01"),
-    sliderInput(inputId = "nTrades", label = "Select Orders number greater than...", value = c(0, 100),min = 0, max = 100)
+# 
+dashboardPage(
+  dashboardHeader(title = "Trading Systems performance evalutaiton"),
+  dashboardSidebar(),
+  dashboardBody(
+    fluidRow(
+      column(1, selectInput(inputId = "TermNum", label = "Select Terminal Number",choices = 1:4)),
+      column(1, actionButton(inputId = "Refresh", label = "refresh")),
+      column(1, selectInput(inputId = "MagicNum", label = "Select Magic Number", choices = 1:10)),
+      column(2, sliderInput(inputId = "filter", label = "Select Profit Levels", min = -10000, max = 10000, value = c(0, 10000))),
+      column(1, dateInput(inputId = "filterDate", label = "Select Orders newer than...", value = "2017-02-01")),
+      column(1, sliderInput(inputId = "nTrades", label = "Select Orders number greater than...", value = c(0, 100),min = 0, max = 100))
     ),
-  
-  mainPanel(
-    plotOutput('plot1'), # overall visual representation of the terminal trades
-    tableOutput('statistics'), # statistics of selected system
-    plotOutput("plot2") # visual representation of selected system
+    hr(),
+    mainPanel(
+        tabsetPanel(
+          tabPanel("All Systems Plot", plotOutput('plot1')),
+          tabPanel("Basic Statistics", tableOutput('statistics')),
+          tabPanel("Visuals of Selected System", plotOutput("plot2"))
+        )  
+    )
   )
 )
+
+
+# 
+# pageWithSidebar(
+#   headerPanel('Systems performance evalutaiton'),
+#   sidebarPanel(
+#     selectInput(inputId = "TermNum", label = "Select Terminal Number",choices = 1:4),
+#     actionButton(inputId = "Refresh", label = "refresh"),
+#     selectInput(inputId = "MagicNum", label = "Select Magic Number", choices = 1:10),
+#     sliderInput(inputId = "filter", label = "Select Profit Levels", min = -10000, max = 10000, value = c(0, 10000)),
+#     dateInput(inputId = "filterDate", label = "Select Orders newer than...", value = "2017-02-01"),
+#     sliderInput(inputId = "nTrades", label = "Select Orders number greater than...", value = c(0, 100),min = 0, max = 100)
+#     ),
+#   
+#   mainPanel(
+#     plotOutput('plot1'), # overall visual representation of the terminal trades
+#     tableOutput('statistics'), # statistics of selected system
+#     plotOutput("plot2") # visual representation of selected system
+#   )
+# )

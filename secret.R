926d0992fb29e4ea14697400df0133a94f06a70a
vzhomeexperiments <vz.home.experiments@gmail.com>
2017-08-21 23:07:28 +0200

    script to install secret package from github

--- secret.R
+++ secret.R
@@ -1,1 +1,8 @@
 # Exploring the secret package
+install.packages("secret") #does not worked...
+
+# this worked out!
+devtools::install_github("gaborcsardi/secret")
+# My secret will be located here:
+
+fakepassword <- "MyFakePassword334455"

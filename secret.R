a2c5444d07e03d3062ac0890c57c2ff61f71c249
vzhomeexperiments <vz.home.experiments@gmail.com>
2017-08-26 22:48:16 +0200

    experimenting with secret package

--- secret.R
+++ secret.R
@@ -1,8 +1,36 @@
-# Exploring the secret package
-install.packages("secret") #does not worked...
+# Logic for keeping secrets in the Vault
 
-# this worked out!
-devtools::install_github("gaborcsardi/secret")
-# My secret will be located here:
+#devtools::install_github("gaborcsardi/secret")
+library(secret)
+#library(openssl)
 
+## creating a vault in the working directory
+# getting working directory to the variable original_wd
+original_wd <- getwd()
+
+# creating a vault named tormos
+dir.create(file.path(original_wd, "tormos"))
+create_vault(file.path(original_wd, "tormos"))
+
+## adding users to the vault
+# getting paths to the keys of the user
+key_dir <- "C:/Users/fxtrams/.ssh"
+pub_key_dir <- file.path(key_dir1, "id_rsa.pub") #directory for public key
+prv_key_dir <- file.path(key_dir1, "id_rsa")
+# reading public key with openssl package:
+openssl::read_pubkey(pub_key_dir)
+# adding users to the vault
+#
+# adding user to the vault myVault1
+add_user("vzhomeexperiments", public_key = pub_key_dir, vault = "tormos")
+add_github_user("vzhomeexperiments", vault = "tormos")
+
+# generating fake secret
 fakepassword <- "MyFakePassword334455"
+
+# add secret to the vault myVault1, share it for both users
+add_secret(name = "TheFakeSecret", value = fakepassword, users = c("vzhomeexperiments",
+                                                                    "github-vzhomeexperiments"), vault = "tormos")
+
+# get that secret usign the private key /user having private key/
+secret_retrieved <- get_secret(name = "TheFakeSecret", key = prv_key_dir, vault = "tormos")
